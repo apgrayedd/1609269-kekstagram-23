3aaf41cc8384e844d18ece91e432aef78e1c4ba2
apgrayedd <56434676+apgrayedd@users.noreply.github.com>
2021-06-01 11:09:30 +0300

    Исправление ошибок

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let getRandomNumber = (min,max) => {
+const getRandomNumber = (min,max) => {
   if (isNaN(min) || isNaN(max)) {
     return "Неверное значение в переменных";
   }
@@ -6,14 +6,15 @@ let getRandomNumber = (min,max) => {
     return Math.floor(Math.random() * (min - max + 1)) + max;
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-let checkCommentError = (comment, maxLen) => {
+};
+const checkCommentError = (comment, maxLen) => {
   return String(comment).length < maxLen ? String(comment) : true;
-}
-let MAX_LENGTH_COMMENT = 10;
+};
+const MAX_LENGTH_COMMENT = 10;
+let randomNumber;
 let comment = "Случайный комментарий";
 
 comment = checkCommentError(comment,MAX_LENGTH_COMMENT);
-randomNumber = getRandomNumber("re",2)
-console.log(randomNumber)
-console.log(comment)
+randomNumber = getRandomNumber("re",2);
+console.log(randomNumber);
+console.log(comment);
