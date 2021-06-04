dfab50e9fc279397dff89230eebe824e244f65b8
apgrayedd <56434676+apgrayedd@users.noreply.github.com>
2021-06-04 11:23:18 +0300

    Корректировка

--- js/main.js
+++ js/main.js
@@ -2,8 +2,7 @@ const MAX_LENGTH_COMMENT = 10;
 const getRandomNumber = (min,max) => Math.floor(Math.random() * (Math.max(max,min) - Math.min(max,min) + 1)) + Math.min(max,min);
 const checkCommentError = (comment, maxLen) => comment.length < maxLen ? comment : false;
 
-const userComment = checkCommentError('Случайный комментарий',MAX_LENGTH_COMMENT);
-const randomNumber = getRandomNumber(5,-1);
+const userComment = 'Случайный комментарий';
 
-console.log(userComment);
-console.log(randomNumber);
+checkCommentError(userComment,MAX_LENGTH_COMMENT);
+getRandomNumber(5,-1);
