cb0704b5a26251262ce1a43af1ecf74155ca4207
apgrayedd <56434676+apgrayedd@users.noreply.github.com>
2021-06-04 10:00:52 +0300

    Обнова

--- js/main.js
+++ js/main.js
@@ -1,17 +1,9 @@
-const getRandomNumber = (min,max) => {
-  if (isNaN(min) || isNaN(max)) {
-    return "Неверное значение в переменных";
-  }
-  return Math.floor(Math.random() * (Math.max(max,min) - Math.min(max,min) + 1)) + Math.min(max,min);
-};
-const checkCommentError = (comment, maxLen) => {
-  return String(comment).length < maxLen ? String(comment) : true;
-};
 const MAX_LENGTH_COMMENT = 10;
-let randomNumber;
-let comment = "Случайный комментарий";
+const getRandomNumber = (min,max) => Math.floor(Math.random() * (Math.max(max,min) - Math.min(max,min) + 1)) + Math.min(max,min);
+const checkCommentError = (comment, maxLen) => comment.length < maxLen ? comment : false;
 
-comment = checkCommentError(comment,MAX_LENGTH_COMMENT);
-randomNumber = getRandomNumber(-5,2);
+const userComment = checkCommentError('Случайный комментарий',MAX_LENGTH_COMMENT);
+const randomNumber = getRandomNumber(5,-1);
+
+console.log(userComment);
 console.log(randomNumber);
-console.log(comment);
