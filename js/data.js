328b0d6a4dd87835fd74f488bd06280ea346175f
apgrayedd <apgrayedd@mail.ru>
2021-06-20 11:19:54 +0300

    Обнова

--- js/data.js
+++ js/data.js
@@ -1,35 +1,28 @@
 import {getRandomNumber} from './util.js';
 
-const checkCommentError = (comment, maxLen) => comment.length < maxLen ? comment : false;
-function mixedArray (array = []) {
-  const mixArray = [];
-  while (array.length > 0) {
-    const randomElem = array.splice(getRandomNumber(0, array.length - 1), 1)[0];
-    mixArray.push(randomElem);
-  }
-  return mixArray;
-}
-function getRandomComment(idComment,comments,names) {
+function getRandomComment(idComment,comments,names, NUMBER_OF_COMMENTS) {
   const linkStrAvatar = 'img/avatar-';
   const typeImgAvatar = '.svg';
-  const message = comments[getRandomNumber(0,comments.length-1)];
-  const message2 = message + comments[getRandomNumber(0,comments.length-1)];
+  const message = comments[getRandomNumber(0,comments.length - 1)];
+  const message2 = message + comments[getRandomNumber(0,comments.length - 1)];
   return {
     id: idComment,
-    avatar: linkStrAvatar + getRandomNumber(1,6) + typeImgAvatar,
+    avatar: linkStrAvatar + getRandomNumber(1,NUMBER_OF_COMMENTS) + typeImgAvatar,
     message: Math.random() < 0.5 ? message: message2,
-    name: names[getRandomNumber(0,names.length-1)],
+    name: names[getRandomNumber(0,names.length - 1)],
   };
 }
-function getRandomPost(idPost,idComments,idPhoto,comments,names, NUMBER_OF_COMMENTS) {
+
+function getRandomPost(idPost,idComments,idPhoto,comments,names,likes, NUMBER_OF_COMMENTS) {
   const linkStrPost = 'photos/';
   const typeImgPost = '.svg';
   return {
     id : idPost,
     url : linkStrPost + idPhoto + typeImgPost,
     description : 'Случайное описание',
-    likes : getRandomNumber(15,200),
+    likes : getRandomNumber(likes.min,likes.max),
     comments : new Array(NUMBER_OF_COMMENTS).fill().map((elem,key) => getRandomComment(idComments[key],comments,names)),
   };
 }
-export {checkCommentError, getRandomPost, getRandomComment, mixedArray};
+
+export {getRandomPost, getRandomComment};
--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-import {checkCommentError, getRandomPost, mixedArray} from './data.js';
-import {getRandomNumber} from './util.js';
+import {getRandomPost} from './data.js';
+import {getRandomNumber, checkCommentError, mixArray} from './util.js';
 
 const MAX_LENGTH_COMMENT = 10;
-const NUMBER_OF_POSTS_AND_PHOTO = getRandomNumber(1,25);
-const NUMBER_OF_COMMENTS = getRandomNumber(1,6);
+const MAX_OF_POSTS_AND_PHOTO = 25;
+const MAX_OF_COMMENTS = 6;
+const likes = {
+  min: 15,
+  max: 200,
+};
 const someComments = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -15,8 +19,8 @@ const someComments = [
 const someNames = [
   'Артем','Владимир','Александр','Виталик','Алексей','Дмитрий',
 ];
-const someIdComments = new Array(NUMBER_OF_COMMENTS).fill().map((elem,key) => key+=1).sort(() => 0.5 - Math.random());
-const someIdPosts = mixedArray(new Array(NUMBER_OF_POSTS_AND_PHOTO).fill().map((elem,key) => key+=1));
-const someIdPhotos = mixedArray(new Array(NUMBER_OF_POSTS_AND_PHOTO).fill().map((elem,key) => key+=1));
-const posts = new Array(NUMBER_OF_POSTS_AND_PHOTO).fill().map((elem,key) => getRandomPost(someIdPosts[key],someIdComments,someIdPhotos[key],someComments,someNames,NUMBER_OF_COMMENTS));
+const someIdComments = mixArray(new Array(getRandomNumber(1,MAX_OF_COMMENTS)).fill().map((elem,key) => key+= 1));
+const someIdPosts = mixArray(new Array(getRandomNumber(1,MAX_OF_POSTS_AND_PHOTO)).fill().map((elem,key) => key+= 1));
+const someIdPhotos = mixArray(new Array(getRandomNumber(1,MAX_OF_POSTS_AND_PHOTO)).fill().map((elem,key) => key+= 1));
+const posts = new Array(getRandomNumber(1,MAX_OF_COMMENTS)).fill().map((elem,key) => getRandomPost(someIdPosts[key],someIdComments,someIdPhotos[key],someComments,someNames,likes,MAX_OF_COMMENTS));
 checkCommentError(posts[0]['comments'][0]['message'],MAX_LENGTH_COMMENT);
