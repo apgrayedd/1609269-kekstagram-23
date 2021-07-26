003f5769353e74698a30299cecf1ef241ba000d8
apgrayedd <56434676+apgrayedd@users.noreply.github.com>
2021-07-26 08:52:49 +0300

    Обнова

--- js/filter.js
+++ js/filter.js
@@ -1,66 +1,66 @@
 import { mixArray } from './util.js';
 
 function addFilterOptions() {
-    const filtersSection = document.querySelector('.img-filters');
-    filtersSection.classList.remove('img-filters--inactive');
+  const filtersSection = document.querySelector('.img-filters');
+  filtersSection.classList.remove('img-filters--inactive');
 }
 
 function filterDefault(postsArr) {
-    return postsArr;
+  return postsArr;
 }
 
 function filterByRandom(postsArr, numberRandomPosts) {
-    return mixArray(postsArr).slice(0, numberRandomPosts);
+  return mixArray(postsArr).slice(0, numberRandomPosts);
 }
 
 function filterByComments(postsArr) {
-    return postsArr.slice().sort((postA, postB) => postB.comments.length - postA.comments.length);
+  return postsArr.slice().sort((postA, postB) => postB.comments.length - postA.comments.length);
 }
 
 const clearPosts = () => {
-    const posts = document.querySelectorAll('.picture');
-    posts.forEach((post) => {
-        post.remove();
-    });
+  const posts = document.querySelectorAll('.picture');
+  posts.forEach((post) => {
+    post.remove();
+  });
 };
 
 function postsFilter(posts, addPostsFunction, maxNumberForRandomFilter, RERENDER_DELAY) {
-    const filterButtons = document.querySelectorAll('.img-filters__button');
-    const filterDefaultButton = document.querySelector('#filter-default');
-    const filterRandomButton = document.querySelector('#filter-random');
-    const filterDiscussedButton = document.querySelector('#filter-discussed');
-    const delClassButtons = () => {
-        for (const button of filterButtons) {
-            if (button.classList.contains('img-filters__button--active')) {
-                button.classList.remove('img-filters__button--active');
-            }
-        }
+  const filterButtons = document.querySelectorAll('.img-filters__button');
+  const filterDefaultButton = document.querySelector('#filter-default');
+  const filterRandomButton = document.querySelector('#filter-random');
+  const filterDiscussedButton = document.querySelector('#filter-discussed');
+  const delClassButtons = () => {
+    for (const button of filterButtons) {
+      if (button.classList.contains('img-filters__button--active')) {
+        button.classList.remove('img-filters__button--active');
+      }
+    }
+  };
+  const filterWidthDebounce = (filter, filterFunct, adds) => {
+    delClassButtons();
+    const filterFunction = () => {
+      clearPosts();
+      addPostsFunction(filterFunct(posts, adds));
     };
-    const filterWidthDebounce = (filter, filterFunct, adds) => {
-        delClassButtons();
-        const filterFunction = () => {
-            clearPosts();
-            addPostsFunction(filterFunct(posts, adds));
-        };
-        const filterWidthDebounce = _.debounce(filterFunction, RERENDER_DELAY);
-        filter.classList.add('img-filters__button--active');
-        filterWidthDebounce();
+    const filterDebounce = _.debounce(filterFunction, RERENDER_DELAY);
+    filter.classList.add('img-filters__button--active');
+    filterDebounce();
 
-    };
-    const filterDefaultHandler = () => filterWidthDebounce(filterDefaultButton, filterDefault);
-    const filterRandomHandler = () => filterWidthDebounce(filterRandomButton, filterByRandom, maxNumberForRandomFilter);
-    const filterDiscussedHandler = () => filterWidthDebounce(filterDiscussedButton, filterByComments);
+  };
+  const filterDefaultHandler = () => filterWidthDebounce(filterDefaultButton, filterDefault);
+  const filterRandomHandler = () => filterWidthDebounce(filterRandomButton, filterByRandom, maxNumberForRandomFilter);
+  const filterDiscussedHandler = () => filterWidthDebounce(filterDiscussedButton, filterByComments);
 
-    addFilterOptions();
-    filterDefaultButton.addEventListener('click', filterDefaultHandler, false);
-    filterRandomButton.addEventListener('click', filterRandomHandler, false);
-    filterDiscussedButton.addEventListener('click', filterDiscussedHandler, false);
+  addFilterOptions();
+  filterDefaultButton.addEventListener('click', filterDefaultHandler, false);
+  filterRandomButton.addEventListener('click', filterRandomHandler, false);
+  filterDiscussedButton.addEventListener('click', filterDiscussedHandler, false);
 }
 
 export {
-    postsFilter,
-    filterByRandom,
-    filterByComments,
-    addFilterOptions,
-    filterDefault
+  postsFilter,
+  filterByRandom,
+  filterByComments,
+  addFilterOptions,
+  filterDefault
 };
--- js/util.js
+++ js/util.js
@@ -1,192 +1,192 @@
 const getRandomNumber = (min, max) => Math.floor(Math.random() * (Math.max(max, min) - Math.min(max, min) + 1)) + Math.min(max, min);
 
 function mixArray(items = []) {
-    const array = [...items];
-    const mixedArray = [];
-    while (array.length > 0) {
-        const randomElem = array.splice(getRandomNumber(0, array.length - 1), 1)[0];
-        mixedArray.push(randomElem);
-    }
-    return mixedArray;
+  const array = [...items];
+  const mixedArray = [];
+  while (array.length > 0) {
+    const randomElem = array.splice(getRandomNumber(0, array.length - 1), 1)[0];
+    mixedArray.push(randomElem);
+  }
+  return mixedArray;
 }
 
 function checkRepeatArr(arr, variableProcessing) {
-    const arrValues = [];
+  const arrValues = [];
 
-    for (let str of arr) {
-        if (variableProcessing && typeof(str) === 'string') {
-            str = variableProcessing(str);
-        }
-
-        if (arrValues.includes(str)) {
-            return false;
-        } else {
-            arrValues.push(str);
-        }
+  for (let str of arr) {
+    if (variableProcessing && typeof(str) === 'string') {
+      str = variableProcessing(str);
     }
-    return true;
+
+    if (arrValues.includes(str)) {
+      return false;
+    } else {
+      arrValues.push(str);
+    }
+  }
+  return true;
 }
 
 function findInObj(obj, objKey, value, objKeyForReturn) {
-    for (const elem in obj) {
-        if (elem[objKey] === value) {
-            if (objKeyForReturn) {
-                return elem[objKeyForReturn];
-            }
-            return true;
-        }
+  for (const elem in obj) {
+    if (elem[objKey] === value) {
+      if (objKeyForReturn) {
+        return elem[objKeyForReturn];
+      }
+      return true;
     }
+  }
 }
 
 function checkFirstSymbol(fieldValue, firstSymbolValue) {
-    if (fieldValue[0] !== firstSymbolValue) {
-        return `Первым знаком поля должен быть знак "${firstSymbolValue}"`;
-    }
+  if (fieldValue[0] !== firstSymbolValue) {
+    return `Первым знаком поля должен быть знак "${firstSymbolValue}"`;
+  }
 }
 
 
 function checkMinLength(fieldValue, MIN_LENGTH_FIELD) {
-    if (fieldValue.length < MIN_LENGTH_FIELD) {
-        return `Поле должен состоять хотя бы из ${MIN_LENGTH_FIELD}х символов`;
-    }
+  if (fieldValue.length < MIN_LENGTH_FIELD) {
+    return `Поле должен состоять хотя бы из ${MIN_LENGTH_FIELD}х символов`;
+  }
 }
 
 function checkMaxLength(fieldValue, MAX_LENGTH_FIELD) {
-    if (fieldValue.length > MAX_LENGTH_FIELD) {
-        return `Поле должен состоять не больше ${MAX_LENGTH_FIELD} символов`;
-    }
+  if (fieldValue.length > MAX_LENGTH_FIELD) {
+    return `Поле должен состоять не больше ${MAX_LENGTH_FIELD} символов`;
+  }
 }
 
 function matchValidation(fieldValue) {
-    const match = fieldValue.match(/[a-zA-Zа-юА-Ю0-9]*/);
+  const match = fieldValue.match(/[a-zA-Zа-юА-Ю0-9]*/);
 
-    if (!match) {
-        return 'Поле не должно содержать #, @, $ и т. п.';
-    }
+  if (!match) {
+    return 'Поле не должно содержать #, @, $ и т. п.';
+  }
 
-    const result = match[0] === match['input'] ? fieldValue : false;
-    if (!result) {
-        return 'Поле не должно содержать #, @, $ и т. п.';
-    }
+  const result = match[0] === match['input'] ? fieldValue : false;
+  if (!result) {
+    return 'Поле не должно содержать #, @, $ и т. п.';
+  }
 }
 
 
 function createFunctionByKeyDown(evt, keyCode, eventFunction) {
-    if (!evt) { evt = window.event; }
-    const evtKeyCode = evt.keyCode;
-    if (evtKeyCode === keyCode) {
-        evt.preventDefault();
-        eventFunction();
-    }
+  if (!evt) { evt = window.event; }
+  const evtKeyCode = evt.keyCode;
+  if (evtKeyCode === keyCode) {
+    evt.preventDefault();
+    eventFunction();
+  }
 }
 
 function createHTMLElement(tag, classArr, addAttributesObj, addStylesObj) {
-    const newElement = document.createElement(tag);
-    if (classArr) {
-        classArr.forEach((classItem) => {
-            newElement.classList.add(classItem);
-        });
-    }
+  const newElement = document.createElement(tag);
+  if (classArr) {
+    classArr.forEach((classItem) => {
+      newElement.classList.add(classItem);
+    });
+  }
 
-    if (addAttributesObj) {
-        for (const addAttributeKey in addAttributesObj) {
-            newElement[addAttributeKey] = addAttributesObj[addAttributeKey];
-        }
+  if (addAttributesObj) {
+    for (const addAttributeKey in addAttributesObj) {
+      newElement[addAttributeKey] = addAttributesObj[addAttributeKey];
     }
+  }
 
-    if (addStylesObj) {
-        for (const styleKey in addStylesObj) {
-            newElement['style'][styleKey] = addStylesObj[styleKey];
-        }
+  if (addStylesObj) {
+    for (const styleKey in addStylesObj) {
+      newElement['style'][styleKey] = addStylesObj[styleKey];
     }
-    return newElement;
+  }
+  return newElement;
 }
 
 function addStyles(HTMLElement, stylesObj) {
-    for (const style in stylesObj) {
-        HTMLElement['style'][style] = stylesObj[style];
-    }
+  for (const style in stylesObj) {
+    HTMLElement['style'][style] = stylesObj[style];
+  }
 }
 
 function createMessageAlert(templateName, buttonOptions) {
-    const body = document.querySelector('body');
-    const template = document.querySelector(`#${templateName}`).content.querySelector(`.${templateName}`);
-    const templateHTMLElem = template.cloneNode(true);
+  const body = document.querySelector('body');
+  const template = document.querySelector(`#${templateName}`).content.querySelector(`.${templateName}`);
+  const templateHTMLElem = template.cloneNode(true);
 
-    body.classList.add('modal-open');
-    body.appendChild(templateHTMLElem);
-    const closeForm = () => {
-        body.classList.remove('modal-open');
-        document.querySelector(`.${templateName}`).remove();
-        // eslint-disable-next-line no-use-before-define
-        window.removeEventListener('keydown', closeFormByEsc, false);
-        // eslint-disable-next-line no-use-before-define
-        document.removeEventListener('click', closeOnAnotherForm, false);
-    };
-    const closeFormByEsc = (evt) => {
-        createFunctionByKeyDown(evt, 27, closeForm);
-    };
-    const closeOnAnotherForm = (evt) => {
-        if (!evt.target.classList.contains(`${templateName}__inner`)) {
-            closeForm();
-        }
-    };
-    if (buttonOptions) {
-        buttonOptions.forEach((buttonOption) => {
-            const messageForm = document.querySelector(`.${templateName}`);
-            const messageButton = messageForm.querySelector(`.${buttonOption.name}`);
-            const functionOnButton = () => {
-                if (buttonOption.function) {
-                    buttonOption.function.forEach((funct) => {
-                        funct(messageButton);
-                    });
-                }
-                closeForm();
-                messageButton.removeEventListener('click', functionOnButton, false);
-            };
-            messageButton.addEventListener('click', functionOnButton, false);
-        });
+  body.classList.add('modal-open');
+  body.appendChild(templateHTMLElem);
+  const closeForm = () => {
+    body.classList.remove('modal-open');
+    document.querySelector(`.${templateName}`).remove();
+    // eslint-disable-next-line no-use-before-define
+    window.removeEventListener('keydown', closeFormByEsc, false);
+    // eslint-disable-next-line no-use-before-define
+    document.removeEventListener('click', closeOnAnotherForm, false);
+  };
+  const closeFormByEsc = (evt) => {
+    createFunctionByKeyDown(evt, 27, closeForm);
+  };
+  const closeOnAnotherForm = (evt) => {
+    if (!evt.target.classList.contains(`${templateName}__inner`)) {
+      closeForm();
     }
-    document.addEventListener('click', closeOnAnotherForm, false);
-    window.addEventListener('keydown', closeFormByEsc, false);
+  };
+  if (buttonOptions) {
+    buttonOptions.forEach((buttonOption) => {
+      const messageForm = document.querySelector(`.${templateName}`);
+      const messageButton = messageForm.querySelector(`.${buttonOption.name}`);
+      const functionOnButton = () => {
+        if (buttonOption.function) {
+          buttonOption.function.forEach((funct) => {
+            funct(messageButton);
+          });
+        }
+        closeForm();
+        messageButton.removeEventListener('click', functionOnButton, false);
+      };
+      messageButton.addEventListener('click', functionOnButton, false);
+    });
+  }
+  document.addEventListener('click', closeOnAnotherForm, false);
+  window.addEventListener('keydown', closeFormByEsc, false);
 }
 
 function loadMessage() {
-    const body = document.querySelector('body');
-    const messageInBody = document.querySelector('.img-upload__message--loading');
-    if (messageInBody) {
-        if (messageInBody.classList.contains('hidden')) {
-            messageInBody.classList.remove('hidden');
-            body.classList.add('modal-open');
-        } else {
-            messageInBody.classList.add('hidden');
-            body.classList.remove('modal-open');
-        }
-        return messageInBody;
+  const body = document.querySelector('body');
+  const messageInBody = document.querySelector('.img-upload__message--loading');
+  if (messageInBody) {
+    if (messageInBody.classList.contains('hidden')) {
+      messageInBody.classList.remove('hidden');
+      body.classList.add('modal-open');
+    } else {
+      messageInBody.classList.add('hidden');
+      body.classList.remove('modal-open');
     }
-    const messageTemplate = document.querySelector('#messages').content;
-    const message = messageTemplate.querySelector('.img-upload__message');
-    body.appendChild(message);
-    return document.querySelector('.img-upload__message--loading');
+    return messageInBody;
+  }
+  const messageTemplate = document.querySelector('#messages').content;
+  const message = messageTemplate.querySelector('.img-upload__message');
+  body.appendChild(message);
+  return document.querySelector('.img-upload__message--loading');
 }
 
 function limitationValue(min, max, value) {
-    return Math.min(Math.max(value, min), max);
+  return Math.min(Math.max(value, min), max);
 }
 
 export {
-    checkRepeatArr,
-    findInObj,
-    getRandomNumber,
-    mixArray,
-    checkFirstSymbol,
-    checkMinLength,
-    checkMaxLength,
-    matchValidation,
-    createFunctionByKeyDown,
-    createHTMLElement,
-    createMessageAlert,
-    limitationValue,
-    addStyles,
-    loadMessage
+  checkRepeatArr,
+  findInObj,
+  getRandomNumber,
+  mixArray,
+  checkFirstSymbol,
+  checkMinLength,
+  checkMaxLength,
+  matchValidation,
+  createFunctionByKeyDown,
+  createHTMLElement,
+  createMessageAlert,
+  limitationValue,
+  addStyles,
+  loadMessage
 };
